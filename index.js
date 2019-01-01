0b1c3f02d30bb8191e4bab27148a3ffd4042497e
Unknown <andre-dede96@hotmail.com>
2019-01-01 01:14:17 -0200

    Commit  01012019_0113

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const methodOveride = require('body-parser')
 const morgan = require('body-parser')
 const path = require('path');
-const Sequelize = require('sequelize')
 
 const app = express()
 
@@ -12,28 +11,10 @@ app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json());
 app.use(methodOveride('_method'))
 
-//Configuração Sequilize
-//const sequelize = new Sequelize('mysql://root:@localhost:3306/sequelize')
-const sequelize = new Sequelize('sequelize','root','',{
-    host : 'localhost',
-    dialect: 'mysql',
-    operatorAliases: Sequelize.Op
-    /*pool:{
-        max:
-        min:
-        acquire:
-        idle
-    }*/
-})
-
-sequelize
-    .authenticate()
-    .then(() => console.log('OK!'))
-    .catch(() => console.log('ERROR!'))
-
 app.set('view engine','pug');
 app.set('views', path.join(__dirname,'src/view'))
 
+require('./src/model/index')
 require('./src/index')(app)
 
 app.listen(3000, ()=>{
--- src/controller/home/home.js
+++ src/controller/home/home.js
@@ -1,3 +1,8 @@
-module.exports = (req, res)=>{
-    return  res.render('home/index');    
+const sequelize = require('./../../model/index')
+const Event = sequelize.import('./../../model/events')
+
+module.exports = (req, res)=> {
+    return  res.render('home/index',{
+        title: "Home Page"
+    });    
 }
