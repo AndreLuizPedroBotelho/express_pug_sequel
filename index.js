e6545d1044d2d7fc082e351c6ed2da814a3880a0
Unknown <andre-dede96@hotmail.com>
2019-01-01 15:29:37 -0200

    Commit  01012019_1529

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const morgan = require('body-parser')
+const morgan = require('morgan')
 const path = require('path');
 
 const app = express()
